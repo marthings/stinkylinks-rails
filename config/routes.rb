ec8d1d1230cbec7e1ebfc903b6b681964006216e
Martin Berggren <martin@mwi.se>
2018-04-29 13:29:47 +0200

    Added links and validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: { registrations: "registrations" }
+  
   resources :users, only: [:index, :show]
-  root 'users#index'
+  resources :links, except: [:show]
+  root 'links#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
